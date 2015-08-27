18f08665596f6977e48b427bdac949565592a827
VBmgk <victorbramigk@gmail.com>
2015-08-27 18:27:53 -0300

    Formating main function

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,16 @@
 int main(void) {
   printf("\nCompilador\n");
   AlexicalAnalizer lexical_an("../src/test_code.lang");
-  if (lexical_an.isDigit('0')) printf("\nit is!!\n");
-  if (lexical_an.isDigit('5')) printf("\nit is!!\n");
-  if (lexical_an.isDigit('9')) printf("\nit is!!\n");
-  if (lexical_an.isDigit('c')) printf("\nit is!!\n"); else printf("\nit's not...\n");
+  if (lexical_an.isDigit('0'))
+    printf("\nit is!!\n");
+  if (lexical_an.isDigit('5'))
+    printf("\nit is!!\n");
+  if (lexical_an.isDigit('9'))
+    printf("\nit is!!\n");
+  if (lexical_an.isDigit('c'))
+    printf("\nit is!!\n");
+  else
+    printf("\nit's not...\n");
 
   /*  t_token token;
 
